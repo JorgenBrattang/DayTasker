3eec39735e93c0ffe5e5ddedad047e8f666c9f74
JorgenBrattang <jorgen.kvist@gmail.com>
2022-11-10 09:40:22 +0000

    Add task form now updates the DB and redirects back to list

--- tasks/forms.py
+++ tasks/forms.py
@@ -8,3 +8,11 @@ class AddTask(forms.ModelForm):
         model = Task
         # fields = '__all__'  <<< --- This will show in the order of creation
         fields = ('name', 'done')
+        labels = {
+            'name': 'What do you want to add? ',
+            'done': 'Is the task done already? '
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.form_method = 'post'
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import AddTask
@@ -12,8 +13,14 @@ def get_tasks_list(request):
 
 
 def add_task_form(request):
-    form = AddTask()
-    context = {
-        'form': form
-    }
-    return render(request, 'tasks/add_task_form.html', context)
+    if request.method == 'POST':
+        form = AddTask(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/tasks/')
+    else:
+        form = AddTask()
+        context = {
+            'form': form
+        }
+        return render(request, 'tasks/add_task_form.html', context)
